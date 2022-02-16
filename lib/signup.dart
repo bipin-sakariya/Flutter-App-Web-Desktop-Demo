d6f2e607593ec047af3b2b9ce6ef0374cdf470ba
mac <mac@Chirag-MAC.local>
2022-02-16 18:49:16 +0530

    make responsive UI

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_macos_demo/main.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatefulWidget {
